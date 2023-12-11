a00c6559e4a4b81c0dbe8b860540b8dc9381f797
Erick Morais e Silva <erickmorais@lampp-it.com.br>
2023-12-11 15:45:38 -0300

    Versão 1.0

--- src/advinhacao.py
+++ src/advinhacao.py
@@ -2,24 +2,35 @@ print("*------------------------------*")
 print("Bem vindo no jogo de Advinhação!")
 print("*------------------------------*")
 
-secret_number = 42
+secret_number = 27
+chances_number = 5
+turn = 1
 
-try_chance = input ("Digite o seu numero: ")
+while(chances_number > 0):
+        print("Tentativa:", turn, "de", chances_number)
+        try_chance = input ("Digite o seu número: ")
+        print("Você digitou:", try_chance)
+        chance = int(try_chance)
 
-print("Você digitou:", try_chance)
+        correct = chance == secret_number
+        bigger = chance > secret_number
+        smaller = chance < secret_number
 
-chance = int(try_chance)
+        if (correct):
+            print("Você acertou, parabéns!")
+            play_again = input("Deseja jogar novamente? (s/n): ")
+            if play_again.lower() != 's':
+                break
+        else:
+            if(bigger):
+                print("Você errou, é uma pena! Seu número foi maior do que o número secreto.")
+            elif(smaller):
+                print("Você errou, é uma pena! Seu número foi menor do que o número secreto.")
 
-correct = chance == secret_number
-bigger = chance > secret_number
-smaller = chance < secret_number
+        turn = turn + 1
 
-if (correct):
-    print("Você acertou, parabéns!")
-else:
-    if(bigger):
-        print("Você errou, é uma pena! Seu número foi maior do que o número secreto.")
-    elif(smaller):
-        print("Você errou, é uma pena! Seu número foi menor do que o número secreto.")
-
-print("Fim de Jogo")
+        if turn > chances_number:
+            print("Limite de tentativas atingido. Fim de jogo")
+            play_again = input("Deseja jogar novamente? (s/n): ")
+            if play_again.lower() != 's':
+                break
